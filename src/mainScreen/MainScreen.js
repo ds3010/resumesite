892aca25804938e7ebf27b80e373ac7a28783553
ds3010 <daniel_seijas3010@hotmail.com>
2023-04-28 23:07:57 -0400

    Changing UX 2

--- src/mainScreen/MainScreen.js
+++ src/mainScreen/MainScreen.js
@@ -171,7 +171,7 @@ const MainScreen = (props) => {
   // }, [props.option[0]]);
 
   return (
-    <div style={{ height: "100%", overflow: "scroll",fontFamily: 'Montserrat', overflow: 'scroll' }}>
+    <div style={{ height: "100%", overflow: "scroll",fontFamily: 'Montserrat',paddingBottom: '72px' }}>
       {props.option[0].titleImage ?
         <div className="container h-30 pt-4 text-center">
           <div className="row">
